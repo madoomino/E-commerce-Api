bab70adb41603b6a17a02f70753d7e8beebde6e9
Mustafa Elsayed <39817130+mustafaelsayed147@users.noreply.github.com>
2023-01-09 02:49:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const homeRouter = require("./routes/homeRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
 const { auth } = require("./middleware/authMiddleware");
@@ -10,7 +9,6 @@ require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(homeRouter);
 app.use("/products", auth, productsRouter);
 app.use("/users", usersRouter);
 module.exports = app;
